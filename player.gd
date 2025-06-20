f007757bcf48a5bdea001d1652318c9172a6a2ff
alan dalmonte <aland96@libero.it>
2025-06-20 18:01:30 +0200

    add health and spawn mobs, add game over screen

--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+signal death
+
+var health = 100.0
+
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
@@ -9,4 +13,12 @@ func _physics_process(delta: float) -> void:
 		happyBoo.play_walk_animation()
 	else:
 		happyBoo.play_idle_animation()
-	pass
+	
+	const DAMAGE = 50.0
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			death.emit()
+	
---
+++ survivor_game.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+func spawn_mob():
+	const MOB = preload("res://mob.tscn")
+	var new_mob = MOB.instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+
+func _on_timer_timeout() -> void:
+	spawn_mob()
+
+
+func _on_player_death() -> void:
+	%GameOver.visible = true
+	get_tree().paused = true
